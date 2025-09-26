478d34050222e0c0158ce3afa7d4e36b926d31a1
Eve <181719992+Evesyy@users.noreply.github.com>
2025-09-26 19:34:41 +0800

    uhhh trying to add a new file
    entirely new code

---
+++ CULLADOS_Jfo33_Exercise1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cullados_jfo3._exercise1;
+
+/**
+ *
+ * @author DELL
+ */
+public class CULLADOS_Jfo33_Exercise1 {
+
+    public static void main(String[] args) {
+        String custName = "Dexter";
+        String itemDesc = "Lego";
+        String message;
+        
+        message = custName + " loves building " + itemDesc;
+        
+        System.out.println(message);
+    }
+}
